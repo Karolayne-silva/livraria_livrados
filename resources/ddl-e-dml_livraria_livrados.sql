40d140e1baa04e5feb64746da26e39da812e3c94
Adrian de Souza <60609409+brazadrian@users.noreply.github.com>
2023-03-29 20:46:16 -0300

    atualizacao dos diagramas de classe e de dados

--- resources/ddl-e-dml_livraria_livrados.sql
+++ resources/ddl-e-dml_livraria_livrados.sql
@@ -1,21 +1,12 @@
--- MySQL Workbench Forward Engineering
-
 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
--- Schema mydb
--- -----------------------------------------------------
--- -----------------------------------------------------
 -- Schema livraria_livrados
 -- -----------------------------------------------------
-
--- -----------------------------------------------------
--- Schema livraria_livrados
--- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `livraria_livrados` DEFAULT CHARACTER SET utf8mb3 ;
-USE `livraria_livrados` ;
+CREATE SCHEMA IF NOT EXISTS `livraria_livrados` DEFAULT CHARACTER SET utf8mb3;
+USE `livraria_livrados`;
 
 -- -----------------------------------------------------
 -- Table `livraria_livrados`.`produto`
@@ -31,68 +22,6 @@ CREATE TABLE IF NOT EXISTS `livraria_livrados`.`produto` (
   PRIMARY KEY (`id`))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb3;
-
-
--- -----------------------------------------------------
--- Table `livraria_livrados`.`cd`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `livraria_livrados`.`cd` (
-  `Produto_id` INT NOT NULL,
-  `cantor` VARCHAR(90) NULL DEFAULT NULL,
-  `produtor` VARCHAR(90) NULL DEFAULT NULL,
-  PRIMARY KEY (`Produto_id`),
-  CONSTRAINT `fk_CD_Produto1`
-    FOREIGN KEY (`Produto_id`)
-    REFERENCES `livraria_livrados`.`produto` (`id`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb3;
-
-
--- -----------------------------------------------------
--- Table `livraria_livrados`.`dvd`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `livraria_livrados`.`dvd` (
-  `Produto_id` INT NOT NULL,
-  `diretor` VARCHAR(90) NULL DEFAULT NULL,
-  PRIMARY KEY (`Produto_id`),
-  CONSTRAINT `fk_DVD_Produto1`
-    FOREIGN KEY (`Produto_id`)
-    REFERENCES `livraria_livrados`.`produto` (`id`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb3;
-
-
--- -----------------------------------------------------
--- Table `livraria_livrados`.`venda`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `livraria_livrados`.`venda` (
-  `id` INT NOT NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb3;
-
-
--- -----------------------------------------------------
--- Table `livraria_livrados`.`itemvenda`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `livraria_livrados`.`itemvenda` (
-  `Venda_id` INT NOT NULL,
-  `Produto_id` INT NOT NULL,
-  `quantidade` INT NOT NULL,
-  `dataVenda` DATE NOT NULL,
-  `desconto` DECIMAL(4,2) NULL DEFAULT NULL,
-  PRIMARY KEY (`Venda_id`, `Produto_id`),
-  INDEX `fk_ItemVenda1_idx` (`Produto_id` ASC) INVISIBLE,
-  INDEX `fk_ItemVenda_idx` (`Venda_id` ASC) INVISIBLE,
-  CONSTRAINT `fk_ItemVenda1`
-    FOREIGN KEY (`Produto_id`)
-    REFERENCES `livraria_livrados`.`produto` (`id`),
-  CONSTRAINT `fk_ItemVenda_Venda`
-    FOREIGN KEY (`Venda_id`)
-    REFERENCES `livraria_livrados`.`venda` (`id`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb3;
-
 
 -- -----------------------------------------------------
 -- Table `livraria_livrados`.`livro`
@@ -110,7 +39,6 @@ CREATE TABLE IF NOT EXISTS `livraria_livrados`.`livro` (
     REFERENCES `livraria_livrados`.`produto` (`id`))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb3;
-
 
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
@@ -138,8 +66,8 @@ INSERT INTO `livraria_livrados`.`produto` (`id`, `preco`, `nome`, `descricao`, `
 -- Inserções na tabela `livraria_livrados`.`livro`
 -- -----------------------------------------------------
 
-INSERT INTO `` (`Produto_id`,`isbn13`,`autor`,`editora`,`sinopse`) VALUES (1,'1234567891234','Clarice Lispector','Cerpe','Brasília amarela');
-INSERT INTO `` (`Produto_id`,`isbn13`,`autor`,`editora`,`sinopse`) VALUES (2,'9897132981739','Bodidharma','Buddah','Carecas da paz');
-INSERT INTO `` (`Produto_id`,`isbn13`,`autor`,`editora`,`sinopse`) VALUES (3,'9874631234132','Yoda Tano','Jorge Luscas','Velozes e furiosos no espaço');
-INSERT INTO `` (`Produto_id`,`isbn13`,`autor`,`editora`,`sinopse`) VALUES (4,'3213123211231','Machado de Assis','Quincas Borba','Caba chato, merecia levar gaia');
+INSERT INTO `livraria_livrados`.`livro` (`Produto_id`,`isbn13`,`autor`,`editora`,`sinopse`) VALUES (1,'1234567891234','Clarice Lispector','Cerpe','Brasília amarela');
+INSERT INTO `livraria_livrados`.`livro` (`Produto_id`,`isbn13`,`autor`,`editora`,`sinopse`) VALUES (2,'9897132981739','Bodidharma','Buddah','Carecas da paz');
+INSERT INTO `livraria_livrados`.`livro` (`Produto_id`,`isbn13`,`autor`,`editora`,`sinopse`) VALUES (3,'9874631234132','Yoda Tano','Jorge Luscas','Velozes e furiosos no espaço');
+INSERT INTO `livraria_livrados`.`livro` (`Produto_id`,`isbn13`,`autor`,`editora`,`sinopse`) VALUES (4,'3213123211231','Machado de Assis','Quincas Borba','Caba chato, merecia levar gaia');
 INSERT INTO `livraria_livrados`.`livro` (`Produto_id`, `isbn13`, `autor`, `editora`, `sinopse`) VALUES ('77', '7777777777777', 'Rafael Moreira dos Anjos', 'Abacaxi', 'Paga o que você me deve');
